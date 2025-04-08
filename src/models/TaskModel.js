523386f2610db6710342f97ff6fc19027ee84afa
earvin-tech <wsl@Earvin>
2025-04-09 00:26:07 +1000

    feat: Task Model

---
+++ src/models/TaskModel.js
@@ -0,0 +1,45 @@
+const { default: mongoose } = require("mongoose");
+
+const taskSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        description: {
+            type: String,
+            default: "",
+        },
+        dueDate: {
+            type: Date,
+        },
+        status: {
+            type: String,
+            enum: ["not-started", "in-progress", "completed", "on-hold"],
+            default: "not-started",
+        },
+        priority: {
+            type: String,
+            enum: ["low", "medium", "high"],
+            default: "medium",
+        },
+        project: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Project",
+            required: true,
+        },
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Task = mongoose.model("Task", taskSchema);
+
+module.exports = { Task };
